858d04c1b4ef0bdeafaa22bfe63549a830e83e46
grinjk1993 <grinjk1993@gmail.com>
2022-12-17 17:59:49 +0200

    commit

--- src/components/ImageContent/ImageContent.js
+++ src/components/ImageContent/ImageContent.js
@@ -9,85 +9,70 @@ import { Typography } from '@mui/material';
 export default function ImageContent() {
   const itemData = [
     {
-      id: 0,
-      img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-      title: 'Bed',
+      img: "../images/cat1.jfif",
+      title: "Shyster",
+      id: 1
     },
     {
-      id: 1,
-      img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-      title: 'Books',
+      img: "../images/cat2.jpg",
+      title: "Lazy",
+      id: 2
     },
     {
-      id: 2,
-      img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-      title: 'Sink',
+      img: "../images/cat3.jfif",
+      title: "Brandy",
+      id: 3
     },
     {
-      id: 3,
-      img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-      title: 'Kitchen',
+      img: "../images/cat4.jpg",
+      title: "Milk",
+      id: 4
     },
     {
-      id: 4,
-      img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-      title: 'Blinds',
+      img: "../images/cat5.jpg",
+      title: "Blinds",
+      id: 5
     },
     {
-      id: 5,
-      img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-      title: 'Chairs',
+      img: "../images/cat6.jpg",
+      title: "Charles",
+      id: 6
     },
     {
-      id: 6,
-      img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-      title: 'Laptop',
+      img: "../images/cat7.jpg",
+      title: "Laptop",
+      id: 7
     },
     {
-      id: 7,
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors',
+      img: "../images/cat8.jpg",
+      title: "Dorothy",
+      id: 8
     },
     {
-      id: 8,
-      img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-      title: 'Coffee',
+      img: "../images/cat9.jpg",
+      title: "Feather",
+      id: 9
     },
     {
-      id: 9,
-      img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-      title: 'Storage',
+      img: "../images/cat10.jpg",
+      title: "Storm",
+      id: 10
     },
     {
-      id: 10,
-      img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-      title: 'Candle',
+      img: "../images/cat11.jpg",
+      title: "Candy",
+      id: 11
     },
     {
-      id: 11,
-      img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-      title: 'Coffee table',
+      img: "../images/cat12.jpg",
+      title: "Sporty",
+      id: 12
     },
     {
-      id: 12,
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors',
+      img: "../images/cat13.jpg",
+      title: "Fluffy",
+      id: 13
     },
-    {
-      id: 13,
-      img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-      title: 'Coffee',
-    },
-    {
-      id: 14,
-      img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-      title: 'Storage',
-    },
-    {
-      id: 15,
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors',
-    }
   ];
 
   return (
